ba9dfe400dcfc145351994fe3521d446748aafc9
Maynagashev <lifesup@gmail.com>
2016-11-02 23:53:08 +0700

    jsonp draft

--- ng-wiki-app.js
+++ ng-wiki-app.js
@@ -4,7 +4,13 @@
 (function () {
 
     var app = angular.module('wiki-app', []);
-    
+
+    app.controller('wiki-api', ['$http', function ($http){
+        $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&callback=JSON_CALLBACK')
+            .success(function mycb(data) {
+            console.log(data);
+        });
+    }]);
 
 
 })();
