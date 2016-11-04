14dcf23f717065c3c97ce3d3a5eecd2c0af9c9a1
Maynagashev <lifesup@gmail.com>
2016-11-05 01:38:29 +0700

    testing some angular stuff, buttons, clicks

--- src/ng-wiki-app.js
+++ src/ng-wiki-app.js
@@ -3,7 +3,7 @@
  */
 (function () {
 
-    var app = angular.module('wiki-app', []);
+    var app = angular.module('wiki-app', ['ngAnimate']);
 
     app.controller('wiki-api', ['$http', function ($http){
         $http.jsonp('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&callback=JSON_CALLBACK')
