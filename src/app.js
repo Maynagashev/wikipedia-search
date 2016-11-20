93370a12efb10bd5c10b595651b31c4bbea48380
Maynagashev <lifesup@gmail.com>
2016-11-21 02:08:34 +0700

    + md autocompelete

--- src/app.js
+++ src/app.js
@@ -11,93 +11,32 @@
         .controller('MainController', MainController);
 
 
-    function MainController (wiki, $scope, $compile, $timeout, $q, $log) {
+    function MainController (wiki, $scope, $compile, $timeout, $q, $log, $http) {
 
         var self = this;
 
-        self.simulateQuery = false;
-        self.isDisabled    = false;
+        $scope.searchText = '';
 
-        // list of `state` value/display objects
-        self.states        = loadAll();
-        self.querySearch   = querySearch;
-        self.selectedItemChange = selectedItemChange;
-        self.searchTextChange   = searchTextChange;
-
-        self.newState = newState;
-
-        function newState(state) {
-            alert("Sorry! You'll need to create a Constitution for " + state + " first!");
-        }
-
-
-        /**
-         * Search for states... use $timeout to simulate
-         * remote dataservice call.
-         */
-        function querySearch (query) {
-            var results = query ? self.states.filter( createFilterFor(query) ) : self.states,
-                deferred;
-            if (self.simulateQuery) {
-                deferred = $q.defer();
-                $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-                return deferred.promise;
-            } else {
-                return results;
-            }
-        }
-
-        function searchTextChange(text) {
-            $log.info('Text changed to ' + text);
-        }
-
-        function selectedItemChange(item) {
-            $log.info('Item changed to ' + JSON.stringify(item));
-        }
-
-        /**
-         * Build `states` list of key/value pairs
-         */
-        function loadAll() {
-            var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-
-            return allStates.split(/, +/g).map( function (state) {
-                return {
-                    value: state.toLowerCase(),
-                    display: state
-                };
+        $scope.autoCompSearch = function(text) {
+            var url = "http://en.wikipedia.org/w/api.php?callback=JSON_CALLBACK&action=opensearch&format=json&search="+text;
+            return $http.jsonp(url).then(function (response) {
+                $log.info(response);
+                return response.data[1].map(function (d) {
+                    return {
+                        value: d,
+                        display: d
+                    };
+                });
             });
-        }
-
-        /**
-         * Create filter function for a query string
-         */
-        function createFilterFor(query) {
-            var lowercaseQuery = angular.lowercase(query);
-
-            return function filterFn(state) {
-                return (state.value.indexOf(lowercaseQuery) === 0);
-            };
-
-        }
-
-
-
-
-
+        };
+ 
 
 
 
 
         $scope.errm = [];
         $scope.resultsFetched = false;
-        $scope.query = '';
+
         $scope.results = [];
         $scope.perPage = 10;
 
@@ -188,7 +127,7 @@
         });*/
 
 
-    };
+    }
 })();
 
 
